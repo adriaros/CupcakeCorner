69c30188cd9eda042ff67768018827ab46d9a554
AdriaRos <adria.ros@elma.care>
2022-02-20 20:40:23 +0100

    main
    - Added project

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Adrià Ros on 20/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // That’s the only place the order will be created – every other screen in our app will be passed that property so they all work with the same data
+    @StateObject var order = Order()
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Adrià Ros on 20/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
